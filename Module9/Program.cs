7ca7677946b95e1863361c15d311e9c58daae875
pavelsannikov <pavelsan95@mail.ru>
2022-10-07 20:18:31 +0500

    Добавьте файлы проекта.

---
+++ Module9/Program.cs
@@ -0,0 +1,48 @@
+﻿
+class Program
+{
+    /// <summary>
+    /// Данная программа запрашивает путь до файла, в котором должно быть число от -100 до 100.
+    /// Затем 100 делится на это число, и результат выводится в консоль.
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Введите полный путь до файла");
+            string path = Console.ReadLine();
+            string[] readText = File.ReadAllLines(path);
+            int my_number = Int32.Parse(readText.FirstOrDefault());
+            if ((readText.Length != 1) ||
+                (my_number < -100) || (my_number > 100))
+                throw new MyException("неправильный файл");
+            Console.WriteLine("Успешное завершение программы. Результат: {0}",100/my_number);
+        }
+        catch (System.DivideByZeroException)
+        {
+            Console.WriteLine("На ноль делить нельзя!");
+        }
+        catch (System.ArgumentException)
+        {
+            Console.WriteLine("Некорректный путь до файла, либо файл пустой");
+        }
+        catch (MyException)
+        {
+            Console.WriteLine("Недопустимое значение числа в файле");
+        }
+        catch (System.FormatException)
+        {
+            Console.WriteLine("Содержимое файла некорректно");
+        }
+        catch (System.IO.FileNotFoundException)
+        {
+            Console.WriteLine("Файл не найден");
+        }
+    }
+    public class MyException : Exception
+    {
+        public MyException(string _exceptionMessage) : base(_exceptionMessage) { }
+    }
+}
+   
---
+++ Module9_2/Program.cs
@@ -0,0 +1,53 @@
+﻿class Program
+{
+    public delegate void Notify(int i);
+    static event Notify CorrectData;
+    static String[] Names = { "Иванов", "Сидоров", "Петров", "Сакмиов", "Козлов" };
+    static void Main(string[] args)
+    {
+        CorrectData += Order;
+        try
+        {
+            int command;
+            if (!Int32.TryParse(Console.ReadLine(), out command))
+            {
+                throw new MyException();
+            }
+            switch (command)
+            {
+                case 1:
+                case 2:
+                    CorrectData.Invoke(command);
+                    break;
+                default:
+                    throw new MyException();
+            }
+            Console.WriteLine("Успешное завершение программы.");
+        }
+        catch (MyException)
+        {
+            Console.WriteLine("Допустимы значение \"1\"и \"2\"");
+        }
+
+    }
+    public class MyException : Exception
+    {
+        public MyException() : base() { }
+    }
+    static public void Order(int i)
+    {
+        IOrderedEnumerable<string> res;
+        if (i == 1)
+        {
+            res = from n in Names orderby n select n;
+        }
+        else
+        {
+            res = Names.OrderByDescending(n => n);
+
+        }
+        foreach (var s in res){
+            Console.WriteLine(s);
+        }
+    }
+}
